c940cdf17f7ccf635d1e3e7973587683bab9b187
Ronit <70330163+asawaronit60@users.noreply.github.com>
2020-10-01 18:09:20 +0530

    Update arraymethod.java

--- array/src/arraymethod.java
+++ array/src/arraymethod.java
@@ -8,14 +8,14 @@ public class arraymethod {
 		// TODO Auto-generated method stub
 		int a[]={2,3,6,4,4,5,4};
 		change(a);
-		for(int c: a){
-			System.out.println("\n a:\t"+c);
-		}
+		
+			System.out.println("\n a:\t"+a);
+		
 
 	}
 public static void change(int x[]){
 	System.out.println("b\t x[]");
-	for(int b=0;b<x.length;b++){
+	for(int b :x){
 		x[b]+=1000;
 		System.out.println(b+"\t"+x[b]);
 	}
